964d1e6cdff69a6e5e20287ee67089b73c113d43
ibebbs <github@bebbs.co.uk>
2019-10-11 17:45:36 +0100

    Moved Timer to uno platform

---
+++ src/Timer/Timer.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading;
+using System.Windows;
+
+namespace Timer.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Common.MainPageViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _viewModel = new Common.MainPageViewModel(new SynchronizationContextScheduler(SynchronizationContext.Current));
+
+            DataContext = _viewModel;
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+
+            _viewModel.Activate();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            _viewModel.Deactivate();
+        }
+    }
+}
