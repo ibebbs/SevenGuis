780185165b95714dd4af13a7dc318644e8bda9a7
ibebbs <github@bebbs.co.uk>
2019-10-11 20:41:13 +0100

    Changed TemperatureConverter to Uno

---
+++ src/TemperatureConverter/TemperatureConverter.Shared/StringToIntConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    public class StringToIntConverter : ConverterBase
+    {
+        public override object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString();
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            switch (value)
+            {
+                case string text: return Int32.TryParse(text, out int number) ? number : UnsetValue;
+                default: return UnsetValue;
+            }
+        }
+    }
+}
