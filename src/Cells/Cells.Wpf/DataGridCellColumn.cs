1b83f573e6c6751fe0aa69df61956fc5492c5c6d
ibebbs <github@bebbs.co.uk>
2019-10-17 15:15:19 +0100

    Moved Cells to Uno platform

---
+++ src/Cells/Cells.Wpf/DataGridCellColumn.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cells
+{
+    public class DataGridCellColumn : DataGridTemplateColumn
+    {
+        public static readonly DependencyProperty CellProperty = DependencyProperty.RegisterAttached("Cell", typeof(object), typeof(DataGridCellColumn), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static object GetCell(DependencyObject obj)
+        {
+            return (object)obj.GetValue(CellProperty);
+        }
+
+        public static void SetCell(DependencyObject obj, object value)
+        {
+            obj.SetValue(CellProperty, value);
+        }
+
+        public DataGridCellColumn(char column)
+        {
+            Column = column;
+        }
+
+        protected override FrameworkElement GenerateElement(DataGridCell cell, object dataItem)
+        {
+            Common.RowViewModel rowViewModel = (Common.RowViewModel)dataItem;
+
+            var element = base.GenerateElement(cell, dataItem);
+
+            SetCell(element, rowViewModel[Column]);
+
+            return element;
+        }
+
+        protected override FrameworkElement GenerateEditingElement(DataGridCell cell, object dataItem)
+        {
+            Common.RowViewModel rowViewModel = (Common.RowViewModel)dataItem;
+
+            var element = base.GenerateEditingElement(cell, dataItem);
+
+            SetCell(element, rowViewModel[Column]);
+
+            return element;
+        }
+
+        public char Column { get; }
+    }
+}
---
+++ src/Cells/Cells.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Cells.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Common.MainPageViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrepareSheet();
+
+            _viewModel = new Common.MainPageViewModel(new SynchronizationContextScheduler(SynchronizationContext.Current));
+
+            DataContext = _viewModel;
+        }
+
+        private void PrepareSheet()
+        {
+            Sheet.Columns.Add(new DataGridTemplateColumn { CellTemplate = (DataTemplate)Resources["rowHeaderTemplate"] });
+
+            for (char column = 'A'; column < 'Z'; column++)
+            {
+                Sheet.Columns.Add(new DataGridCellColumn(column) { Header = column, CellTemplate = (DataTemplate)Resources["cellTemplate"], CellEditingTemplate = (DataTemplate)Resources["cellEditingTemplate"] });
+            }
+
+            Sheet.FrozenColumnCount = 1;
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+
+            _viewModel.Activate();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            _viewModel.Deactivate();
+        }
+    }
+}
