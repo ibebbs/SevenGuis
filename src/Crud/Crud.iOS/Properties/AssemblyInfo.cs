853ac7043261342201e0ba2f099c2c154f28bf48
ibebbs <github@bebbs.co.uk>
2019-10-17 13:30:34 +0100

    Moved Crud to Uno platform

--- src/Crud/Crud.iOS/Properties/AssemblyInfo.cs
+++ src/Crud/Crud.iOS/Properties/AssemblyInfo.cs
@@ -5,14 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Crud")]
+[assembly: AssemblyTitle("Crud.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Crud")]
+[assembly: AssemblyProduct("Crud.iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("166de4ca-8f11-4ef9-bcf8-3e7834988e7d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
